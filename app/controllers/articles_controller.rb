6440335a69b21b38b4e727727654d278f8bc2f78
PGrecoo <pasquale.alex10@gmail.com>
2020-02-17 15:26:13 +0100

    Un bel pò di lezioni su log in & logout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,10 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def index
-        @articles = Article.all
+        @articles = Article.paginate(:page => params[:page])
     end
 
     def new
@@ -11,7 +13,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article successfully created"
             redirect_to article_path(@article)
@@ -50,8 +52,16 @@ class ArticlesController < ApplicationController
         def set_article
             @article = Article.find(params[:id])
         end
+
         def article_params
             params.require(:article).permit(:title, :description)
+        end
+        
+        def require_same_user
+            if current_user != @article.user
+                flash[:danger] = "You can only edit or delete your own article"
+                redirect_to root_path
+            end    
         end
 
 end
